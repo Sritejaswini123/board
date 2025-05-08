d0e9d4ff2fd250b7951256d4a0a7687516455f8b
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-05-08 07:43:36 +0530

    implemneting the schemas

--- src/factory.ts
+++ src/factory.ts
@@ -1,15 +1,11 @@
 import type { PinoLogger } from "hono-pino";
-
 import { createFactory } from "hono/factory";
-
 interface Variables {
   logger: PinoLogger;
 }
-
 interface AppBindings {
   Variables: Variables;
 }
-
 const factory = createFactory<AppBindings>();
 
 export default factory;

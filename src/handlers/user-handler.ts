4960b5f4f535436c0f0f39cf45d9bddd74bd033f
shivaji2121 <shivarkv123@gmail.com>
2025-05-09 15:20:12 +0530

    fixed post api

--- src/handlers/user-handler.ts
+++ src/handlers/user-handler.ts
@@ -1,22 +1,12 @@
-<<<<<<< Updated upstream
+import { ZodError } from "zod";
 import { USER_CREATED, USER_FOUND, USER_ID_REQUIRED, USER_NOT_FOUND } from "../constants copy/app-messages.js";
 import { INTERNAL_SERVER_ERROR } from "../constants copy/http-status-codes.js";
 import { BAD_REQUEST, CREATED, NOT_FOUND, OK } from "../constants/http-status-codes.js";
 import { UNPROCESSABLE_ENTITY } from "../constants/http-status-phrases.js";
 import { type NewUser, type User, users } from "../database/schemas/users.js";
 import factory from "../factory.js";
+import { createRecord } from "../service/base-db-services.js";
 import { createUser, getUserById } from "../service/user-service.js";
-=======
-
-import { ZodError } from "zod";
-import { USER_CREATED, USER_DELETEED, USER_FOUND, USER_ID_REQUIRED, USER_NOT_FOUND } from "../constants/app-messages.js";
-import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR, OK } from "../constants/http-status-codes.js";
-import { UNPROCESSABLE_ENTITY } from "../constants/http-status-phrases.js";
-import { type NewUser, type User, users } from "../database/schemas/users.js";
-import factory from "../factory.js";
-import { saveRecord } from "../service/baseservices.js";
-import { deleteUserById, getAllUsers, getUserById } from "../service/user-service.js";
->>>>>>> Stashed changes
 import { sendResponse } from "../utils/send-response.js";
 import { vCreateUser } from "../validations/user-validations.js";
 
@@ -29,11 +19,7 @@ export const createUserHandlers = factory.createHandlers(async (c) => {
     const userData: NewUser = {
       ...validUserReq
     }
-<<<<<<< Updated upstream
-    const user = await createUser<User>(users,userData);
-=======
-    const user = await saveRecord<User>(users, userData);
->>>>>>> Stashed changes
+    const user = await createRecord<User>(users,userData);
     return sendResponse(c, CREATED, USER_CREATED, user);
   } catch (error) {
 
@@ -64,35 +50,3 @@ export const getUserByIdHandlers = factory.createHandlers(async (c) => {
 
 
 
-<<<<<<< Updated upstream
-=======
-//get all users
-export const getAllUsersHandlers = factory.createHandlers(async (c) => {
-  try {
-
-    const usersData = await getAllUsers();
-    return sendResponse(c, OK, USER_FOUND, usersData);
-  } catch (error) {
-    return sendResponse(c, INTERNAL_SERVER_ERROR, USER_NOT_FOUND);
-  }
-});
-
-
-
-
-//delete user by id
-export const deleteUserByIdHandlers = factory.createHandlers(async (c) => {
-  try {
-    const userId = Number(c.req.param('user_id'));
-
-    const deletedUser = await deleteUserById(userId);
-
-    return sendResponse(c, OK, USER_DELETEED, deletedUser);
-
-  } catch (error) {
-
-    return sendResponse(c, INTERNAL_SERVER_ERROR, USER_NOT_FOUND);
-  }
-
-});
->>>>>>> Stashed changes
--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -4,7 +4,6 @@ import { getRecordById } from "./base-db-services.js";
 
 //save user
 export const createUser=async (userData: NewUser)=>{
-<<<<<<< Updated upstream
   const  user =await db.insert(users).values(userData).returning();
   return user[0];
 }
@@ -14,30 +13,3 @@ export const createUser=async (userData: NewUser)=>{
     return await getRecordById(users, userId);
   };
   
-=======
-    const  user =await db.insert(users).values(userData).returning();
-    return user[0];
-  }
-
-
-
-//get user by id
-  export const getUserById = async (userId: number) => {
-    return await getRecordById(users, userId);
-};
-
-
-
-//get all users 
-export const getAllUsers = async () => {
-  return await getAllRecords(users);
-}
-
-//delete user by id
-export const deleteUserById = async (userId: number) => {
-  return await deleteRecordById(users, userId);
-};
-  
-  
-
->>>>>>> Stashed changes
